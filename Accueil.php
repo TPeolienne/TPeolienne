33b5270af0f009f6de7615c590669aaa1ae900c6
Yogenn <a.villemon@lyon.ort.asso.fr>
2017-03-08 15:53:16 +0100

    Dépot 1
    Debut accueil

---
+++ Accueil.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Accueil</title>
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" href="Accueil.css"/>
+	</head>
+		<header>
+			<p>
+				<ul class="ul2">
+					<li><a href="inscription.html">&nbsp;Inscription</a></li>
+					<li><a href="Connexion.html">Connexion&nbsp;/</a></li>
+				</ul>
+			</p>
+			
+			
+		
+			<p><h1>TP éolienne</h1></p>
+		
+			<p>
+			<div class="table">
+				<nav class="nav">
+					<ul class="ul">
+						<li class="menu-acc"><a href="Accueil.html">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Accueil&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+						<li class="menu-acc"><a href="Contact.html">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sites&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+						<li class="menu-acc"><a href="Site.html">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Eoliennes&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+					</ul>
+				</nav>
+			</div>
+			</p>
+		</header>
+	<body>
+	<article>
+			<section id="Accueil">
+				<h1>Infos</h1>
+				<div class="dep">
+					<p>Text</p>
+				</div>
+			</section>
+		</article>
+		
+	</body>
+</html>
